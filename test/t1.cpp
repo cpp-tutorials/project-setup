630039a1255e63b703f39a8e07c9ac059b4e13d7
Florian Warg <=>
2018-06-18 10:27:37 +0200

    Add testing example

---
+++ test/t1.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+namespace {
+    TEST(Test1, T1) {
+        EXPECT_EQ(1, 1);
+    }
+    TEST(Test1, T2) {
+        EXPECT_EQ(2, 2);
+    }
+}
---
+++ test/t2.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+namespace {
+    TEST(Test2, T1) {
+        EXPECT_EQ(1, 1);
+    }
+    TEST(Test2, T2) {
+        EXPECT_EQ(2, 2);
+    }
+}
